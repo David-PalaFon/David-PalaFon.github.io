bcafb46404dda404f10fd1d9cb8b18eb59433ee0
David Palacios Fontecha <116884718+David-PalaFon@users.noreply.github.com>
2023-07-25 13:50:38 +0200

    Delete src directory

--- src/app/components/website-info/website-info.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
-
-@Component({
-  selector: "app-website-info",
-  templateUrl: "./website-info.component.html",
-  styleUrls: ["./website-info.component.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class WebsiteInfoComponent {}
--- src/app/models/comment.ts
+++
@@ -1,8 +0,0 @@
-export class Comments {
-    commentId: string;
-    blogId: string;
-    email: string;
-    commentedBy: string;
-    content: string;
-    commentDate: any;
-}
